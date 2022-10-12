2cba296899b1576a8b8fe3272c47ee67df823dd2
Gulempinchenko Maksim <gulempin@gmail.com>
2022-10-12 16:21:12 +0300

    example 005 if else

--- example_005_if_else/Program.cs
+++ example_005_if_else/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите ваша имя: ");
+string username = Console.ReadLine();
+ 
+
+if(username == "Маша")
+{
+    Console.Write ("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Здрасте ");
+    Console.Write (username);
+}
