1d1a60d7ecc5e10e2f9a1a9f945bc90613722fd0
Gulempinchenko Maksim <gulempin@gmail.com>
2022-10-17 04:23:04 +0300

    added example 10

---
+++ example_010_array_task/Program.cs
@@ -0,0 +1,15 @@
+﻿int [] array = {1,2,3,4,5,6,7,18};
+int find = 18;
+int index = 0;
+int n = array.Length;
+
+while(index < n )
+{
+    if(array[index] == find)
+    {
+  
+    Console.WriteLine(index);
+  
+    }
+    index++;
+}
